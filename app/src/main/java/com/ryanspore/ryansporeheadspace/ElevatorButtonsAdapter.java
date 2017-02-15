0d65eb74a158bd6640a18e54f1784681d8ccddc6
Ryan Spore <ry4n1m3@gmail.com>
2017-02-15 11:38:15 -0800

    Remove non-functional close doors button

--- app/src/main/java/com/ryanspore/ryansporeheadspace/ElevatorButtonsAdapter.java
+++ app/src/main/java/com/ryanspore/ryansporeheadspace/ElevatorButtonsAdapter.java
@@ -7,10 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class ElevatorButtonsAdapter extends RecyclerView.Adapter {
-
-    private static final int FLOOR_BUTTON = 1;
-    private static final int CLOSE_DOORS_BUTTON = 2;
+public class ElevatorButtonsAdapter extends RecyclerView.Adapter<ElevatorButtonsAdapter.FloorNumberHolder> {
 
     private final int floorCount;
     private Delegate delegate;
@@ -21,42 +18,26 @@ public class ElevatorButtonsAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public int getItemViewType(int position) {
-        if(position < floorCount) {
-            return FLOOR_BUTTON;
-        } else {
-            return CLOSE_DOORS_BUTTON;
-        }
-    }
-
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ElevatorButtonsAdapter.FloorNumberHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         TextView itemView = (TextView) layoutInflater.inflate(R.layout.floor_control_cell, parent, false);
-        if(viewType == FLOOR_BUTTON) {
-            return new FloorNumberHolder(itemView);
-        } else {
-            return new CloseDoorsHolder(itemView);
-        }
+        return new FloorNumberHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
-        if(holder instanceof FloorNumberHolder) {
-            final int floor = floorCount - position;
-            final FloorNumberHolder floorNumberHolder = (FloorNumberHolder) holder;
-            floorNumberHolder.setDisplayInt(floor);
-            floorNumberHolder.itemView.getBackground().setColorFilter(null);
-            floorNumberHolder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    delegate.addSelection(floor);
-                    int lightTransparentBlue = 0x889999ff;
-                    floorNumberHolder.itemView.getBackground().setColorFilter(lightTransparentBlue, PorterDuff.Mode.MULTIPLY);
-                    floorNumberHolder.itemView.setOnClickListener(null);
-                }
-            });
-        }
+    public void onBindViewHolder(final ElevatorButtonsAdapter.FloorNumberHolder holder, int position) {
+        final int floor = floorCount - position;
+        holder.setDisplayInt(floor);
+        holder.itemView.getBackground().setColorFilter(null);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delegate.addSelection(floor);
+                int lightTransparentBlue = 0x889999ff;
+                holder.itemView.getBackground().setColorFilter(lightTransparentBlue, PorterDuff.Mode.MULTIPLY);
+                holder.itemView.setOnClickListener(null);
+            }
+        });
     }
 
     @Override
@@ -78,13 +59,6 @@ public class ElevatorButtonsAdapter extends RecyclerView.Adapter {
 
         public void setDisplayInt(int displayInt) {
             itemView.setText(String.valueOf(displayInt));
-        }
-    }
-
-    public static class CloseDoorsHolder extends RecyclerView.ViewHolder {
-        public CloseDoorsHolder(TextView itemView) {
-            super(itemView);
-            itemView.setText("> <");
         }
     }
 
